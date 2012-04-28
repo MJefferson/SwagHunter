b508a5dba0b24efccd7e0d9c469f5634b0df0049
Marq Jefferson <marq.d.jefferson@gmail.com>
2012-04-28 12:38:10 -0400

    working copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swaghacker::Application.config.secret_token = '0bf5392b72ac2b1d893300a92274ffb275b06d097c7186989d88fe7390074c97ce74021557b994e9eec1c50ff8939e534582a81c54fd36284ab0f31e5c995160'
